01c187e17a92c1787aff6d5f095327cb77343db5
Jeff Marshall <jeff@classy.co>
2013-11-25 04:49:13 -0500

    Only check if an email is in use if User is new.

--- models/user.js
+++ models/user.js
@@ -55,15 +55,19 @@ User.prototype.validate = function validateUser(callback){
     return callback(validation_error, null);
   }
 
-  var email = self.tmp.doc_body.email;
+  if (!self.tmp.doc_body._rev){
+    var email = self.tmp.doc_body.email;
 
-  User.identify(email, function(identification_error, user){
-    if (user){
-      return callback(errors.emailInUse(email), null);
-    }
+    return User.identify(email, function(identification_error, user){
+      if (user){
+        return callback(errors.emailInUse(email), null);
+      }
 
-    return Doc.prototype.validate.call(self, callback);
-  });
+      return Doc.prototype.validate.call(self, callback);
+    });
+  }
+
+  return Doc.prototype.validate.call(self, callback);
 }
 
 
